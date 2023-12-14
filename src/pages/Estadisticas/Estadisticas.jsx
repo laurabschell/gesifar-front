0899c301e9ef0298283ec25282b9ff3fdfbcca7a
Laura Schell <52552246+laurabschell@users.noreply.github.com>
2023-12-14 15:00:49 -0300

    fix estaticas

--- src/pages/Estadisticas/Estadisticas.jsx
+++ src/pages/Estadisticas/Estadisticas.jsx
@@ -22,7 +22,6 @@ const Estadisticas = () => {
 
     return (
         <Layout title="Estadisticas">
-            
             <div className={style.container} >
                 <div className={style.containerChart}>
                     <h4>Profesionales registrados por Area</h4>
@@ -30,10 +29,12 @@ const Estadisticas = () => {
                         series={[
                             {
                                 data: [
-                                    { id: 0, value: 2, label: 'Traumatologia' },
-                                    { id: 1, value: 1, label: 'Radiologia' },
-                                    { id: 2, value: 5, label: 'Cirugia General' },
-                                    { id: 3, value: 1, label: 'Internacion' },
+                                    { id: 0, value: professionals.filter(item => item.area === 'Hematologia').length, label: 'Hematologia' },
+                                    { id: 1, value: professionals.filter(item => item.area === "Nefrologia").length, label: 'Nefrologia' },
+                                    { id: 2, value: professionals.filter(item => item.area === "Oncologia").length, label: 'Oncologia' },
+                                    { id: 3, value: professionals.filter(item => item.area === "Neumologia").length, label: 'Neumologia' },
+                                    { id: 4, value: professionals.filter(item => item.area === "Rehabilitacion").length, label: 'Rehabilitacion' },
+                                    { id: 5, value: professionals.filter(item => item.area === "Cardiologia").length, label: 'Cardiologia' },
                                 ],
                             },
                         ]}
@@ -47,20 +48,30 @@ const Estadisticas = () => {
                         xAxis={[
                             {
                                 id: 'barCategories',
-                                data: ['Medico', 'Enfermero', 'Medica', 'Enfermera'],
+                                data: ['Medico', 'Radiologo', 'Enfermero', 'Anestesista', 'Cirujano'],
                                 scaleType: 'band',
                             },
                         ]}
                         series={[
                             {
-                                data: [2, 3, 1, 2],
+                                data: [
+                                    professionals.filter(item => item.profesion === "Medico").length,
+                                    professionals.filter(item => item.profesion === "Radiologo").length,
+                                    professionals.filter(item => item.profesion === "Enfermero").length,
+                                    professionals.filter(item => item.profesion === "Anestesista").length,
+                                    professionals.filter(item => item.profesion === "Cirujano").length,
+                                ],
                             },
                         ]}
-                        width={450}
+                        width={500}
                         height={200}
                     />
                 </div>
 
+
+            </div>
+            <div className={style.banner}>
+                <i class="fa-solid fa-chart-line"></i><p>En total hay <strong>{professionals.length}</strong> profesionales solicitantes registrados en la aplicacion</p>
             </div>
         </Layout>
     )
